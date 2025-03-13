77e7e5a844e35da533d34ee795196ad9820d0227
Sulakshan69 <168500936+Sulakshan69@users.noreply.github.com>
2025-03-13 15:07:50 +0545

    Update Question No B

--- coursework1/Question no B.cpp
+++ coursework1/Question no B.cpp
@@ -8,14 +8,13 @@ int main() {
     int maxNum, randomNumber, guess;
     char difficulty;
 
-    // Seed the random number generator
     srand(time(0));
 
-    // Ask user to choose difficulty
+   
     cout << "Choose difficulty - Easy, Medium, Hard: ";
     cin >> difficulty;
 
-    // Set range based on difficulty
+   
     if (difficulty == 'E' || difficulty == 'e')
         maxNum = 8;
     else if (difficulty == 'M' || difficulty == 'm')
@@ -27,10 +26,9 @@ int main() {
         return 1;
     }
 
-    // Generate random number in the selected range
     randomNumber = (rand() % maxNum) + 1;
 
-    // Start guessing game
+   
     cout << "Guess the number between 1 and " << maxNum << ": ";
     do {
         cin >> guess;
